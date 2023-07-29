1e11cade158fbcbacb85e96d7829253ff2a34255
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-29 21:33:57 +0600

    add static variable and method code

---
+++ PracticeProject/src/staticVariable/Method/Main.java
@@ -0,0 +1,11 @@
+package staticVariable.Method;
+
+
+public class Main{
+    public static void main(String[] args){
+
+        System.out.println(myClass.add());
+        // System.out.println(myClass.number);
+
+    }
+}
---
+++ PracticeProject/src/staticVariable/Method/myClass.java
@@ -0,0 +1,13 @@
+package staticVariable.Method;
+
+public class myClass{
+    // public static int number = 10;
+
+    static void add(){
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("Sum is: "+c);
+    }
+
+}
