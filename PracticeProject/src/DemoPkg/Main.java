64e4fb2a73c21c2690ec95b5ad00e0203412d811
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-29 20:06:09 +0600

    add class object creating code

---
+++ PracticeProject/src/DemoPkg/Main.java
@@ -0,0 +1,9 @@
+package DemoPkg;
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass ObjectVariable = new MyClass();
+        ObjectVariable.sayHello();
+        ObjectVariable.welcomeMessage();
+    }
+}
---
+++ PracticeProject/src/DemoPkg/MyClass.java
@@ -0,0 +1,10 @@
+package DemoPkg;
+
+public class MyClass {
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+    public void welcomeMessage(){
+        System.out.println("Welcome to JAVA's World!!");
+    }
+}
