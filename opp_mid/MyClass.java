2de6ce0fe6316897ce60ada5be220934789a1f56
mdyasinahmed <yasinahmed7875@gmail.com>
2023-10-15 21:33:29 +0600

    add oops practice code

---
+++ opp_mid/MyClass.java
@@ -0,0 +1,17 @@
+// What is Class?
+    /**
+     *  Class is a user defined data type that act as the blueprint for objects, attributes, methods.
+     */
+
+// What is Object?
+    /**
+     * Object is the instance of class with specifically defined data.
+     * There are a lot of elements in a class, we can get access of the elements from the class through object.
+     * */
+
+
+
+public class MyClass {
+}
+
+
