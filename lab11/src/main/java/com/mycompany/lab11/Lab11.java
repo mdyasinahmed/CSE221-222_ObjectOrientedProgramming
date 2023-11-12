d884eecb35a788feb1ab9586f14984a6c918b4c5
mdyasinahmed <yasinahmed7875@gmail.com>
2023-11-12 23:17:36 +0600

    add lab task-11 solve code

---
+++ lab11/src/main/java/com/mycompany/lab11/Lab11.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab11;
+
+/**
+ *
+ * @author YASIN
+ */
+public class Lab11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
